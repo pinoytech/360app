995d28f6e3a339b7833d8c78dc38f5059358883e
Ayra Panganiban <ayra.panganiban@gmail.com>
2014-03-21 15:27:16 +0800

    activate season

--- app/models/season.rb
+++ app/models/season.rb
@@ -3,4 +3,16 @@ class Season < ActiveRecord::Base
   CLOSED = 'closed'
   scope :open,  -> { where('status=?', OPEN) }
   scope :closed,  -> { where('status=?', CLOSED) }
+
+  has_many :exams
+
+  before_save :activate_seasons
+
+  def active?
+    self.status.eql?(OPEN)
+  end
+
+  def activate_seasons
+    Season.open.update_all(status: CLOSED) if self.active?
+  end
 end
