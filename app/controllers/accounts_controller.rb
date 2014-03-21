66e61a5a3420276081030655c5d3f27fe2a2c9b4
Ayra Panganiban <ayra.panganiban@gmail.com>
2014-03-22 03:04:24 +0800

    admin

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -21,4 +21,30 @@ class AccountsController < ApplicationController
     @user.save
     redirect_to :back
   end
+
+  def new
+    @account = Account.new
+  end
+
+  def create
+    @account = Account.new(allowed_params)
+    respond_to do |format|
+      if @account.save
+        format.html { redirect_to accounts_path, notice: 'Company Account was successfully created.' }
+      else
+        format.html { render action: 'new' }
+      end
+    end
+  end
+
+  def delete_account
+    @user = User.find(params[:user_id])
+    @user.update_attributes(account_id: nil)
+    redirect_to account_path(params[:id])
+  end
+
+  private
+  def allowed_params
+    params.require(:account).permit :name, :description, :email, :telephone, :address
+  end
 end
