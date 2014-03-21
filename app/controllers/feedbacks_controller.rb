3973bfc980091a28b1e6e4852bb45486bf169856
Thorpe J.Y. Obazee <forum.providence@yahoo.com>
2014-03-22 04:10:15 +0800

    provided feedbacks panel done

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,5 +1,6 @@
 class FeedbacksController < ApplicationController
   def index
     @pending_feedback = current_user.exams_users.incomplete
+    @provided_feedbacks = current_user.exams_users.complete
   end
 end
