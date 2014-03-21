97147f0b5c9517eaa8e1c4cd138bafeaea6d87fe
Ayra Panganiban <ayra.panganiban@gmail.com>
2014-03-21 12:43:52 +0800

    receiver and sender

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
-  belongs_to :sender, class_name: 'User', foreign_key: :user_id
-  belongs_to :receiver, class_name: 'User', foreign_key: :from_id
+  belongs_to :sender, class_name: 'User', foreign_key: :from_id
+  belongs_to :receiver, class_name: 'User', foreign_key: :user_id
 
   scope :public,  -> { where('messages.share=?', true) }
   scope :personal, -> { where('messages.share=?', false) }
