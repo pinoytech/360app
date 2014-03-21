afc5a75e933a256e9c6660e616d2c0d62ddb8748
miguelito <migz.suelto@gmail.com>
2014-03-22 07:05:13 +0800

    feedback for you

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,6 +2,7 @@ class FeedbacksController < ApplicationController
   def index
     @pending_feedback = current_user.exams_users.incomplete
     @provided_feedbacks = current_user.exams_users.complete
+    @received_feedbacks = ExamsUser.where(ratee_id: current_user.id, completed: true)
   end
 
   def show
