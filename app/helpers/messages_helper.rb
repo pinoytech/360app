888828134bce4f62c73c544e5d9f64c0b312ccc7
Ayra Panganiban <ayra.panganiban@gmail.com>
2014-03-21 13:10:08 +0800

    sender helper

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,5 +1,5 @@
 module MessagesHelper
   def privacy_text(message)
-    message.share? ? 'This message is visible to all' : "This message is visible only to you and #{message.from.first_name}"
+    message.share? ? 'This message is visible to all' : "This message is visible only to you and #{message.sender.first_name}"
   end
 end
