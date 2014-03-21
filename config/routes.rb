5c1a15615f0c41a15aebc4d625ce77ed8b8238de
Ayra Panganiban <ayra.panganiban@gmail.com>
2014-03-21 12:27:33 +0800

    admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Hackathon2014::Application.routes.draw do
   devise_for :users
+  root to: 'home#dashboard'
+  
   resources :questions
   resources :categories
   resources :exams
   resources :messages
-  root to: 'home#dashboard'
-  match 'dashboard' => 'home#dashboard', via: :get
 
-  resources :messages
+  match 'dashboard' => 'home#dashboard', via: :get
+  match '/admin/dashboard' => 'admin#index', via: :get
 end
