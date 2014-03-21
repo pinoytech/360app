739dc13e4175c8fd57378c64432e32fef840c5ce
Ayra Panganiban <ayra.panganiban@gmail.com>
2014-03-21 21:55:39 +0800

    avatar/registration

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -10,4 +10,9 @@ module HomesHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def welcome(user)
+    name = user.full_name.blank? ? user.email : user.full_name.titleize
+    "Welcome #{name}!"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
       :small => "200x200>",
       :thumb => "100x100>"
     },
-    :default_url => "/images/:style/missing.png"
+    :default_url => "/avatar/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   has_many :messages
