58d42f6c38880542e7b1e520f4e7af673f8f1f92
Thorpe J.Y. Obazee <forum.providence@yahoo.com>
2014-03-21 15:44:08 +0800

    badges and seed for badges

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,49 @@ User.create(first_name: 'Ayra', last_name: 'Panganiban', email: 'ayrap@sourcepad
 User.create(first_name: 'Admin', last_name: 'User', email: 'admin@sourcepad.com', password: '12345678', admin: 1)
 
 Message.create(title: 'Hello World!', body: 'Please take time to praise someone.', badge_id: 1, from_id: 2, user_id: 1)
+
+designs = [
+    {
+      url: "http://i.imgur.com/OD79Ok5.png",
+      name: 'Client Honor Badge',
+      description: 'Outstanding especially when dealing with clients'
+    },
+    {
+      url: "http://i.imgur.com/BxiJWDu.png",
+      name: 'Deep Thought Badge',
+      description: 'For those who solves hard and complex questions'
+    },
+    {
+      url: "http://i.imgur.com/BCvp6nT.png",
+      name: 'Thank You Badge',
+      description: 'A general \'thank you\' badge'
+    },
+    {
+      url: "http://i.imgur.com/23HE5RM.png",
+      name: 'Finisher Badge',
+      description: 'For people who wrap things and get things done!'
+    },
+    {
+      url: "http://i.imgur.com/FbAxD7E.png",
+      name: 'Teamplayer Badge',
+      description: 'Awesome teamplayer and help us succeed as a team'
+    },
+    {
+      url: "http://i.imgur.com/yWV0Fk9.png",
+      name: 'Innovator Badge',
+      description: 'For people who come up with innovative solutions and insights'
+    },
+    {
+      url: "http://i.imgur.com/J2FpJw6.png",
+      name: 'Thumbs Up Badge',
+      description: 'For people who come up with innovative solutions and insights'
+    },
+]
+
+designs.each do |design|
+    Badge.create(
+      name: design[:name],
+      description: design[:description],
+      image: open(design[:url])
+    )
+end
