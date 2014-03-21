4294b2b2f512dcbf8bc63b08e8a05e9e61fa0401
Ayra Panganiban <ayra.panganiban@gmail.com>
2014-03-21 11:18:22 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackathon2014::Application.config.secret_key_base = '0abe781d5472772543773294935d754ce09c9a72e047367cff25bb884dd98173e04195932a9ee8628bdba6bede1d7848043db52408fae9de119edf97e754f5b2'
