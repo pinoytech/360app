96e41285da28ecb3a4af9bb139071db48524d2e0
miguelito <migz.suelto@gmail.com>
2014-03-21 20:28:14 +0800

    edit, update and remove exam

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,6 +1,7 @@
 class Exam < ActiveRecord::Base
   belongs_to :season
-  has_many :questions, through: :exam_questions
+  has_many :questions, through: :exams_questions
+  has_many :exams_questions
   has_many :users, through: :exams_users
   has_many :exams_users
 end
