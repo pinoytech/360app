5aa7f16b6df04b0efe01e6ab397f1b110db85e47
Thorpe J.Y. Obazee <forum.providence@yahoo.com>
2014-03-21 12:12:58 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Hackathon2014::Application.routes.draw do
   devise_for :users
+  resources :questions
+  resources :categories
+  resources :exams
+  resources :messages
   root to: 'home#dashboard'
   match 'dashboard' => 'home#dashboard', via: :get
 end
