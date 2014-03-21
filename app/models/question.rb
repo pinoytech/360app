c73a7e1abf72174dae8dd17c9dc1b492f5055456
Gino Cortez <gino.carlo.cortez@gmail.com>
2014-03-21 22:19:07 +0800

    - Add exam user formz

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :category
-  has_many :exams, through: :exam_questions
+  has_many :exams_questions
+  has_many :exams, through: :exams_questions
+
+  self.inheritance_column = :fake_column
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Hackathon2014::Application.routes.draw do
       get :category_questions
       get :select_questions
       get :remove_questions
-      get :question_list 
+      get :question_list
     end
   end
   resources :seasons
@@ -24,4 +24,12 @@ Hackathon2014::Application.routes.draw do
   resources :badges
   match 'dashboard' => 'home#dashboard', via: :get
   match '/admin/dashboard' => 'admin#index', via: :get
+
+  resources :exams_users do
+    collection do
+      get :responses
+      post :create_response
+    end
+  end
+
 end
